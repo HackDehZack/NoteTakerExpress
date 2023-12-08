f58e56589d0646b721c291152d65a9b62dafea9c
HackDehZack <Zackseriousemail@gmail.com>
2023-12-07 22:14:44 -0500

    getting things running with server.js

--- server.js
+++ server.js
@@ -1,21 +1,68 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-
-
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // Add this line for JSON parsing
 
 // Serve the notes.html file
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-  });
-  
-  // Serve the index.html file for all other routes
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
 
-  app.listen(PORT, () => {
-    console.log(`App listening on PORT ${PORT}`);
+// Serve the index.html file for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// API Routes
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to retrieve notes' });
+    }
+
+    const notes = JSON.parse(data);
+    res.json(notes);
   });
+});
+
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+
+  if (!title || !text) {
+    return res.status(400).json({ error: 'Title and text are required' });
+  }
+
+  fs.readFile('./db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to save note' });
+    }
+
+    const notes = JSON.parse(data);
+    const newNote = {
+      id: uuidv4(),
+      title,
+      text,
+    };
+    notes.push(newNote);
+
+    fs.writeFile('./db.json', JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to save note' });
+      }
+
+      res.json(newNote);
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
